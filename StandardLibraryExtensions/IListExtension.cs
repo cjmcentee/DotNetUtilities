895c873a196fa374d5d0a2bc38514882fd88ac6d
Colton McEntee <cjmcentee@gmail.com>
2015-02-05 22:02:09 -0800

    Max Value method
    Added static method that returns the maximum value from a list, analogue
    to MinValue

--- StandardLibraryExtensions/IListExtension.cs
+++ StandardLibraryExtensions/IListExtension.cs
@@ -32,5 +32,30 @@ namespace StandardLibraryExtensions
 
             return minIndex;
         }
+
+        public static T MaxValue<T>(this IList<T> list, Func<T, double> valueFunction) {
+            return list[list.IndexOfMax(valueFunction)];
+        }
+
+        public static int IndexOfMax<T>(this IList<T> list, Func<T, double> valueFunction) {
+            if (list == null) throw new ArgumentNullException("list");
+
+            IEnumerator<T> enumerator = list.GetEnumerator();
+            bool isEmptyList = !enumerator.MoveNext();
+
+            if (isEmptyList) throw new ArgumentOutOfRangeException("list", "list is empty");
+
+            int minIndex = 0;
+            double maxValue = valueFunction(enumerator.Current);
+            for (int i = 1; enumerator.MoveNext(); ++i) {
+                double currentValue = valueFunction(enumerator.Current);
+                if (currentValue > maxValue) {
+                    maxValue = currentValue;
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
     }
 }
